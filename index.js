f9012a1860abf8b41a02eca897d0ef7edd36352a
IGNACIO UTRERA <ignacio.utrera@uc.cl>
2023-06-04 00:30:33 -0400

    trabajo sabado

--- index.js
+++
@@ -1,6 +0,0 @@
-const dotenv = require('dotenv');
-const app = require('./app');
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const KoaLogger = require('koa-logger');
 const { koaBody } = require('koa-body');
 const dotenv = require('dotenv');
 const router = require('./routes');
+const orm = require('./models');
 // const cors = require('koa2-cors');
 
 dotenv.config();
 
 const app = new Koa();
+
+app.context.orm = orm;
 
 app.use(KoaLogger());
 app.use(koaBody());
