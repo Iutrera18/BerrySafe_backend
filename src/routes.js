0b2496c67d7015f0019684682398973a95bebc1d
Iutrera18 <ignacio.utrera@uc.cl>
2023-06-05 14:28:56 -0400

    endpoints

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,38 @@ const router = new Router();
 
 router.post('/uploadimg', async (ctx) => {
   // Codigo para subir una foto a BDD
+  try {
+    const newImage = await ctx.orm.image.create(ctx.request.body);
+    ctx.body = newImage;
+    ctx.status = 201;
+  } catch (error) {
+    ctx.status = 400;
+    ctx.body = error;
+  }
 })
 
-router.get('/img/:id', async (ctx) => {
+router.get('/img/:link', async (ctx) => {
   // Codigo para mostrar una foto por id
+  try {
+    const imageurl = await ctx.orm.Image.findOne({where: {url: ctx.params.link}});
+    if (imageurl) {
+      ctx.body = imageurl;
+    } else {
+      ctx.body = "not-found";
+    }
+    ctx.status = 201
+  } catch (error) {
+    ctx.body = error;
+    ctx.status = 400;
+  }
 })
 
 router.get('/savedimgs', async (ctx) => {
   // Codigo para mostrar todas las fotos
   try {
-    ctx.body = "Bien";
+    const urls = await ctx.orm.Image.findAll();
+    ctx.body = {report: "Bien",
+    imgs: urls};
     ctx.status = 201;
   } catch (error) {
     ctx.body = error;
@@ -21,7 +43,20 @@ router.get('/savedimgs', async (ctx) => {
   }
 })
 
-router.delete('/delete/:id', async (ctx) => {
+router.delete('/delete/:link', async (ctx) => {
   // Codigo para borrar imagen de BDD
+  try {
+    const imageToDelete = await ctx.orm.Image.findOne({where: {url: ctx.params.link}});
+    if (imageToDelete) {
+      imageToDelete.destroy();
+      ctx.body = "Imagen eliminada exitosamente";
+    } else {
+      ctx.body = "not-found";
+    }
+    ctx.status = 201;
+  } catch (error) {
+    ctx.body = error;
+    ctx.status = 400;
+  }
 })
 module.exports = router;
